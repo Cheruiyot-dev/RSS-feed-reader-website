0b93f53cf5e91ab128e984cb52665c901f61dfed
Cheruiyot-dev <allankipkoech9@gmail.com>
2023-02-12 21:16:04 +0300

    New changes

--- RSS/views.py
+++ RSS/views.py
@@ -1,44 +1,37 @@
 from django.shortcuts import render
 import feedparser
 
-
+def short_description(description, length):
+    short_desc = description[:length] + '...'
+    return [short_desc[i:i + length] for i in range(0, len(short_desc), length)]
 
 def index(request):
-    # if request.method == 'POST':
-    #     url = request.POST.get('url')
+    if request.method == 'POST':
+        url = request.POST.get('url')
 
+        d = feedparser.parse(url)
     
-    # url = input('enter url link:')
-    url = 'https://www.standardmedia.co.ke/rss/headlines.php'
+        entries = []
+        for item in d['entries']:
+# .get() method checks whether the title key exists in the item before it is accessed.
+#  If it doesn't exist,a default value is set, which is returned.This  allows you to specify a default value
+#  that will be returned if the key you're looking for doesn't exist.
 
-# print(url)
-
-    d = feedparser.parse(url)
-    title = d['feed']['title']
-
-    entries = []
-
-    # try:
-    #     print(title)
-
-    # except:
-    #     print('There is no source title')
-
-
-    for item in d['entries']:
-        title = item['title']
-        description = item['description']
-        link = item['link']
-
-        entries.append({
-            'title': title,
-            'description': description,
-            'link': link
-        })
+            title = item.get('title', 'No title available')
+            description = item.get('description', 'No description available')
+            short_desc = short_description(description, 80)
+            link = item.get('link', 'No link available')
+            entries.append({
+                'title': title,
+                'description': short_desc,
+                'link': link
+            })
         
-    context = {
-        'entries':entries,
-        'feed_title': d['feed']['title']
+        context = {
+            'entries':entries,
+            'feed_title': d.get('feed', {}).get('title', 'No feed title available') 
         
-    }
-    return render(request, 'RSS/index.html', context)
+        }
+        return render(request, 'RSS/index.html', context)
+    return render(request, 'RSS/index.html')
+
